30c884d646f8324f12a3d07bd973e4f2c103a807
Hema c m <hemacm72@gmail.com>
2021-10-17 11:00:17 +0530

    number pattern assignment

---
+++ basic_assignment_numberpattern/Numberpattern5.java
@@ -0,0 +1,12 @@
+class Numberpattern5 {
+	public static void main(String[] args) {
+		int row=7;
+		for(int i=1;i<=row;i++){
+			for(int j=7;j>=i;j--){
+				System.out.print(j);
+				
+			}
+		System.out.println();
+	}
+}
+}
---
+++ basic_assignment_numberpattern/Numberpattern6.java
@@ -0,0 +1,11 @@
+class Numberpattern6 {
+	public static void main(String[] args) {
+		int row=7;
+		for (int i=row;i>=1 ;i--){
+			for (int j=row;j>=i ;j--){
+				System.out.print(j);
+			}
+		System.out.println();
+	}
+}
+}
